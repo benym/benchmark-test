2b0704a630191ade12dd6970bddb64881a839503
benym <benyuanming@gmail.com>
2022-11-14 14:16:08 +0800

    init

---
+++ rpas-test/src/main/java/com/benym/benchmark/test/RpasTestApplication.java
@@ -0,0 +1,13 @@
+package com.benym.benchmark.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpasTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpasTestApplication.class, args);
+    }
+
+}
