c91e638d1d0eb32cf0ed3b5ee02f744e4a96f19e
benym <benyuanming@gmail.com>
2022-12-05 16:25:37 +0800

    feature:mh exception benchmark

--- rpas-test/src/main/java/com/benym/benchmark/test/MhExceptionBenchMark/MethodAccessor.java
+++ rpas-test/src/main/java/com/benym/benchmark/test/MhExceptionBenchMark/MethodAccessor.java
@@ -8,30 +8,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 /**
- * MethodHandle在各类开源框架中大量使用，如Mybatis等
- * 为什么使用MethodHandle
- * MethodHandle性能压测如何
- *
- * @link {<a href="https://www.optaplanner.org/blog/2018/01/09/JavaReflectionButMuchFaster.html">...</a>}
- * <p/>
- * 由于在类外进行私有变量的访问
- * 在JDK8环境下
- * MethodHandle虽然单独有效，但和LambdaMetafactory.metafactory BiConsumer很难结合在一起
- * @link {<a href="https://stackoverflow.com/questions/28184065/java-8-access-private-member-with-lambda">...</a>}
- * 一种更hack的结合方法是采用MethodHandle的内部方法，但由于安全性和运行原理未知，暂不使用
- * @link {<a href="https://stackoverflow.com/questions/69068124/lambdametafactory-and-private-methods">...</a>}
- * <p/>
- * 在JDK9环境下MethodHandle提供privateLookupIn则才能和LambdaMetafactory.metafactory结合
- * <p/>
- * tip: 即使不采用LambdaMetafactory方式做到更为通用的MethodHandle, 如果使用时不采用static，则MethodHandle甚至慢于反射
- * 简单的静态化MethodHandle仍然是通过底层native执行方法，压测性能接近原生，远快于反射
- * <p/> 采用纳秒为单位，数值越小越快
- * Benchmark                                              Mode  Cnt    Score    Error  Units
- * MethodHandleTest.MHBenchmark.testNoStaticMethodHandle  avgt   10  732.150 ± 40.476  ns/op
- * MethodHandleTest.MHBenchmark.testNoStaticReflection    avgt   10  439.412 ±  8.547  ns/op
- * MethodHandleTest.MHBenchmark.testStaticMethodHandle    avgt   10    1.561 ±  0.014  ns/op
- * MethodHandleTest.MHBenchmark.testStaticReflection      avgt   10   25.693 ±  0.543  ns/op
- *
+ * 对于非私有变量访问的场景，MethodHandle可以和LambdaMetafactory.metafactory任意lambda函数结合，做到更通用化，此处为和Function结合
+ * 采用纳秒为单位，数值越小越快，从上至下，直接new set、lambda mh、无lambda的mh
  * Benchmark                                      Mode  Cnt     Score     Error  Units
  * MhExceptionBenchMark.MhExceptioTest.directNew  avgt   10   154.341 ±  16.426  ns/op
  * MhExceptionBenchMark.MhExceptioTest.mhLamda    avgt   10   274.854 ±  13.552  ns/op
